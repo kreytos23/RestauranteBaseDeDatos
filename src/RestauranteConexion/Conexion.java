7fb20a79c09f0d0b8e51b836b082c2dc9ace5e26
Cesar Martin <kreytos.pavi10@gmail.com>
2021-01-06 20:12:57 -0600

    Paneles de Menu de Admin Y User

--- src/RestauranteConexion/Conexion.java
+++ src/RestauranteConexion/Conexion.java
@@ -7,8 +7,9 @@ import java.sql.SQLException;
 public class Conexion {
     public Connection getConnection(){
         Connection conection = null;
+        
         try {
-            conection = DriverManager.getConnection("jdbc:mysql://localhost:3306/technoteam?serverTimezone=UTC","root","PasswordSQL");
+            conection = DriverManager.getConnection("jdbc:mysql://localhost:3306/technoteam?serverTimezone=UTC","root","PasswordSQL.");
         }catch (SQLException e) {
             System.out.println(e);
         }
