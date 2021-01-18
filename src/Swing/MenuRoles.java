a500059b9d1a9bf037d5a634fb877086410e1ae2
Cesar Martin <kreytos.pavi10@gmail.com>
2021-01-18 00:02:29 -0600

    Tickets

--- src/Swing/MenuRoles.java
+++ src/Swing/MenuRoles.java
@@ -1,5 +1,9 @@
 
 package Swing;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 /**
  * @author serpi
  */
@@ -16,6 +20,7 @@ public class MenuRoles extends javax.swing.JPanel {
     private void initComponents() {
 
         btnIniciarSesion = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         lbFondo = new javax.swing.JLabel();
 
@@ -38,6 +43,15 @@ public class MenuRoles extends javax.swing.JPanel {
         add(btnIniciarSesion);
         btnIniciarSesion.setBounds(1030, 40, 260, 80);
 
+        jLabel2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jLabel2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel2MouseClicked(evt);
+            }
+        });
+        add(jLabel2);
+        jLabel2.setBounds(730, 50, 50, 40);
+
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/MenuRoles/EncabezadoPanel.png"))); // NOI18N
         jLabel1.setText("jLabel1");
         add(jLabel1);
@@ -59,10 +73,26 @@ public class MenuRoles extends javax.swing.JPanel {
        this.updateUI();
     }//GEN-LAST:event_btnIniciarSesionActionPerformed
 
+    private void jLabel2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MouseClicked
+        if(java.awt.Desktop.isDesktopSupported()){
+           java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
+           
+           if(desktop.isSupported(java.awt.Desktop.Action.BROWSE)){
+               try{
+                   java.net.URI uri = new java.net.URI("https://www.facebook.com/Restaurante-3-Panaderos-110684760689198");
+                   desktop.browse(uri);
+               } catch(URISyntaxException | IOException ex){
+                   
+               }
+           }
+       }
+    }//GEN-LAST:event_jLabel2MouseClicked
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnIniciarSesion;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel lbFondo;
     // End of variables declaration//GEN-END:variables
 }
